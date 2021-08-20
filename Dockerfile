5353c07e34e4523f6f13b8eac46bdd3593742df0
Ibad Shah <iamibadd>
2021-08-21 02:43:48 +0500

    Subject: Docker support added.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM node:16-alpine
 # best practice
 USER root
-# copy all files from host to source (container)
-COPY . /
 # execution folder
-WORKDIR /
+WORKDIR .
 # port
 ENV PORT 5000
+# look for any change in package.json file and then only run the below two commands
+COPY package.json .
 # install latest node version
 RUN npm -g install npm@latest
 # install dependencies
 RUN npm install
+# copy all files from host to source (container)
+COPY . .
 # starts the application
 CMD node app.js
 #CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-require('dotenv/config')
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+require('dotenv/config');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 const routes = require('./src/routes');
 mongoose.connect(process.env.DB_CONNECTION,
 	{useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}, (err, client) => {
-		if (err) return console.log(err)
-		console.log('Connected to db')
+		if (err) return console.log(err);
+		console.log('Connected to db');
 	})
-app.use(express.json())
-app.use('/', routes)
-app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
+app.use(express.json());
+app.use('/', routes);
+app.use('/health', (req, res) => {
+	res.send('Server is healthy!!');
+});
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
